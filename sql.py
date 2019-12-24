f43acc707e39fa5b9da6d6beb21b33e14818a23d
MOping99 <moping2@uic.edu>
2019-12-23 20:05:53 -0600

    1

--- sql.py
+++ sql.py
@@ -4,6 +4,8 @@ def main():
     con = sqlite3.connect("test.db")
     cur = con.cursor()
 
-    con.execute("""CREATE TABLE samples (id integer, value text)""")
+    #con.execute("""CREATE TABLE samples (id integer, value text)""")
+    con.execute("INSERT INTO samples VALUES (1, 'abc')")
+    con.commit()
 
 main()
